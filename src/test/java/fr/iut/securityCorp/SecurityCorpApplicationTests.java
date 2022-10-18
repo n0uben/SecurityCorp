67bba58080fe328415d8464e6aaa5edb36617b19
n0uben <benjamin.noureux@gmail.com>
2022-10-18 15:07:38 +0200

    Spring initializer avecSpring web et JDBC data API

---
+++ src/test/java/fr/iut/securityCorp/SecurityCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.securityCorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
